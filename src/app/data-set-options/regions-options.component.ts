a945f81357da5ab98b5418f495bae337821a252b
Michael Rotoloni <michael.rotoloni@moodys.com>
2018-08-08 16:38:28 -0700

    Added documentation for the regions component.

--- src/app/data-set-options/regions-options.component.ts
+++ src/app/data-set-options/regions-options.component.ts
@@ -8,12 +8,35 @@ import { Regions } from '../regions.model';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegionsOptionsComponent {
+
+  /**
+   * The available regions to display.
+   *
+   * @type {Regions[]}
+   * @memberof RegionsOptionsComponent
+   */
   @Input() public regions: Regions[];
 
+  /**
+   * Event emitter that emits when the list of selected regions change.
+   *
+   * @memberof RegionsOptionsComponent
+   */
   @Output() public changeRegions = new EventEmitter<Regions[]>();
 
+  /**
+   * Region input reactive form control.
+   *
+   * @memberof RegionsOptionsComponent
+   */
   public regionControl = new FormControl();
 
+  /**
+   * Event handler for when the available regions is changed in the UI.
+   *
+   * @param {string[]} ids
+   * @memberof RegionsOptionsComponent
+   */
   public onRegionsChange(ids: string[]) {
     throw new Error('Missing Logic');
   }
